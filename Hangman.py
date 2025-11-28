acea79988b3ef7deb4876409d9482f2b7858ea00
rylan-gif <139591221+rylan-gif@users.noreply.github.com>
2025-11-28 17:14:50 -0600

    Update Hangman.py, got rid of redundancy, and added some explanations for readability

--- Hangman.py
+++ Hangman.py
@@ -9,13 +9,14 @@ word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 WORDS = response.content.splitlines()
 # choosing random word
+
 wordSlice = str(random.choice(WORDS))
 word = wordSlice[2:-1]
 
-
+#creating the encoded word given to user
 encodedWord = ["_"] * len(word)
 polePhase=0
-
+#ASCII for hangman pole itself
 poleStart = ("      - - - - - -\n "
              "      |          |\n"
              "       |\n"
@@ -65,6 +66,7 @@ death = ("      - - - - - - \n"
          "      |         / \\\n"
          "      |\n"
          "- - - - - - -\n")
+
 root = tk.Tk()
 # create window with base geometry
 root.title("Hangman")
@@ -74,7 +76,7 @@ root.maxsize(500, 500)
 root.geometry("300x300+50+50")
 
 
-# create our initial window before game starts
+#Function for adding characters that are not within our word
 def appendingChars(character):
     current = charList.get()
     # do not want repeats of characters in this
@@ -82,13 +84,15 @@ def appendingChars(character):
         new = current + character
         charList.set(new)
 
-# create labels
+# Title
 tk.Label(root, text="Hangman", fg="white", bg="black").pack()
-
+#Encoded word
 encoded = tk.Label(root, text=" ".join(encodedWord), bg="black", fg="white")
 encoded.place(x=100, y=300)
+#entry box that the user types guesses into
 entry = tk.Entry(root, font=("Helvetica", 14))
 entry.pack(pady=20)
+#where the letters that are NOT in the word are displayed
 exludedBank = tk.Label(root, text="letters: ", bg="black", fg="white")
 exludedBank.place(x=300, y=300)
 charList = tk.StringVar(value="")  # since no characters were grabbed, this set should be empty
@@ -96,13 +100,13 @@ excluded = tk.Label(root, textvariable=charList, bg="black", fg="white")
 excluded.place(x=300, y=325)
 # actual game time now:
 
-
+#when game ends this is function to quit
 def stop():
     global cont
     cont = False
     root.destroy()
 
-
+#when game ends if the user decides to keep playing this function happens
 def repeat():
     python = sys.executable
     # uses subprocess.Popen to launch new instance of current script
@@ -110,11 +114,14 @@ def repeat():
     subprocess.Popen([python] + sys.argv)
     sys.exit(0)
 
-
+#every time submit guess button is pressed
 def guess():
+    #initialize global variables
     global polePhase
     global encodedWord
+    #grab user input
     input = entry.get()
+    #if user just straight up types the encoded word
     if input == word:
         for widget in root.winfo_children():
             widget.destroy()
@@ -122,22 +129,30 @@ def guess():
         quitButton.place(x=300, y=100)
         restart = tk.Button(root, text="Restart!", command=repeat)
         restart.place(x=50, y=100)
-        how = tk.Label(root, text="Amazing work! continue?")
-        how.place(x=100, y=100)
-
+        how = tk.Label(root, text="Amazing work! continue?", fg="white", bg="black")
+        how.place(x=150, y=100)
+    #if they place characters instead or just giving which characters in the word the user typed are not contained in the word
     else:
+        #iterate over each character in user input
         for char in input:
+            #if guessed character is not in the word
             if char not in word:
+                #adding characters to excluded list
                 appendingChars(char)
                 polePhase += 1
+            #some character is in the word
             else:
-                if char in word:
-                    for index, letter in enumerate(word):
-                        if char == letter:
-                            encodedWord[index] = char
-                    encoded.configure(text=" ".join(encodedWord))
-
+                #for each character in the word
+                for index, letter in enumerate(word):
+                    #check if character is the letter in the word
+                    if char == letter:
+                        #switch out the blank for our character
+                        encodedWord[index] = char
+                #re-create encoded word
+                encoded.configure(text=" ".join(encodedWord))
+                #check to see if our word is the actual word
                 if ("".join(encodedWord) == word):
+                    #clear screen and create 2 buttons that we will offer to either restart game, or quit game
                     for widget in root.winfo_children():
                         widget.destroy()
                     quitButton = tk.Button(root, text="Quit", command=stop)
@@ -145,7 +160,8 @@ def guess():
                     restart = tk.Button(root, text="Restart!", command=repeat)
                     restart.place(x=50, y=100)
                     encouragement = tk.Label(root, text="Good job on getting it right!", fg="white", bg="black")
-                    encouragement.place(x=100, y=100)
+                    encouragement.place(x=150, y=100)
+        #logic for setting the pole phases of the game
         if polePhase == 1:
             phase.config(text=pole1)
         elif polePhase == 2:
@@ -156,6 +172,7 @@ def guess():
             phase.config(text=pole4)
         elif polePhase == 5:
             phase.config(text=pole5)
+        #user loses
         if polePhase == 6:
             for widget in root.winfo_children():
                 widget.destroy()
@@ -169,11 +186,11 @@ def guess():
             DEADMAN.place(x=200, y=200)
             CorrectWord = tk.Label(root, text=f"The correct word was {word}", fg="white", bg="black")
             CorrectWord.place(x=100, y=50)
-
+#submit guess
 button = tk.Button(root, text="submit guess", command=guess)
 button.place(x=200, y=100)
-
+#create our phases of our poles
 phase = tk.Label(root, text=poleStart, bg="black", fg="white", justify="left")
 phase.place(x=100, y=100)
-# create the window
+#loop
 root.mainloop()
